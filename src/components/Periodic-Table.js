fc666c03eaa3881f10329b8d77ca3dafe80ffb58
Breyanna Vass <breyannavass@gmail.com>
2019-09-17 11:39:55 -0400

    trying to use props for displaying other info

--- src/components/Periodic-Table.js
+++ src/components/Periodic-Table.js
@@ -24,25 +24,25 @@ class PeriodicTable extends Component {
     return this.state.elements.filter(element => element.name.toUpperCase());
   };
 
-  hoverOver = element => {
-    let elementInfo = this.state.elements;
-
-    switch (elementInfo) {
-      case 0:
-        elementInfo = element.atomicRadius;
-        break;
-      case 1:
-        elementInfo = element.electronegativity;
-        break;
-      case 2:
-        elementInfo = elementInfo.meltingPoint;
-        break;
-      case 3:
-        elementInfo = element.bondingType;
-        break;
-      default:
-        return element.name;
-    }
+  hoverOver = ({ element }) => {
+    // let elementInfo = this.state.elements;
+    // alert(elementInfo);
+    // switch (elementInfo) {
+    //   case 0:
+    //     elementInfo = element.atomicRadius;
+    //     break;
+    //   case 1:
+    //     elementInfo = element.electronegativity;
+    //     break;
+    //   case 2:
+    //     elementInfo = elementInfo.meltingPoint;
+    //     break;
+    //   case 3:
+    //     elementInfo = element.bondingType;
+    //     break;
+    //   default:
+    //     return element.name;
+    // }
   };
 
   render() {
