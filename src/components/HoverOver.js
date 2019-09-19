a4319b91eefe78c71d47e1844042b440882d3a72
Breyanna Vass <breyannavass@gmail.com>
2019-09-19 13:14:05 -0400

    Adding MDBMask to hoveroverjs

--- src/components/HoverOver.js
+++ src/components/HoverOver.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { MDBMask, MDBView } from "mdbreact";
 
 const HoverOver = ({ element }) => (
-  <div className="ElementCard">
-    <p>The atomic radius is {element.atomicRadius}</p>
-    <p>The boiling point is {element.boilingPoint}</p>
-    <p>The standard state is {element.standardState}</p>
-    <p>It was discovered in {element.yearDiscovered}</p>
-  </div>
+  <MDBView hover zoom>
+    <div className="ElementCard">
+      <MDBMask overlay="black-light">
+        <p>The atomic radius is {element.atomicRadius}</p>
+        <p>The boiling point is {element.boilingPoint}</p>
+        <p>The standard state is {element.standardState}</p>
+        <p>It was discovered in {element.yearDiscovered}</p>
+      </MDBMask>
+    </div>
+  </MDBView>
 );
 
 export default HoverOver;
